ca992e5c3ee26c2bae106c753aaeda70cbb97587
wouter-droog <wouter_730@hotmail.com>
2019-01-29 12:37:27 +0100

    Add BurgerBuilder container

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
-
-import './App.css';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Layout>  
-          <p>Hallo</p>
+          <BurgerBuilder></BurgerBuilder>
         </Layout>
       </div>
     );
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import layoutStyles from './Layout.module.css';
+
 const layout = (props) => (
     <>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className={layoutStyles.Content}>
             {props.children}
         </main>
     </>
