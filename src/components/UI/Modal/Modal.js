c7627cc0a0dd501637abebce59313fe32a40ec5d
wouter-droog <wouter_730@hotmail.com>
2019-02-06 10:37:10 +0100

    Improved performance: ordersummary only updated when Modal is shown

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -3,16 +3,24 @@ import Backdrop from '../Backdrop/Backdrop';
 
 import modalStyles from './Modal.module.css';
 
-const modal = (props) => (
-    <>
-        <Backdrop clicked={props.modalClosed} show={props.show} />
-        <div className={modalStyles.Modal} style={{
-            transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-            opacity: props.show ? '1' : '0'
-        }}>
-            {props.children}
-        </div>
-    </>
-);
+class modal extends React.Component {
+    // This prevents updating the ordersummary when the modal is not shown
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
+
+    render() {
+
+        return <>
+            <Backdrop clicked={this.props.modalClosed} show={this.props.show} />
+            <div className={modalStyles.Modal} style={{
+                transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                opacity: this.props.show ? '1' : '0'
+            }}>
+                {this.props.children}
+            </div>
+        </>;
+    }
+}
 
 export default modal;
