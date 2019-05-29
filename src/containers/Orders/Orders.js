eea50f2bb8c6ff47de79ed46b1313bc61e358337
Droog <wdroog84@gmail.com>
2019-05-29 13:16:29 +0200

    Render comment

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -14,6 +14,7 @@ class Orders extends Component {
         this.props.onFetchOrders();
     }
 
+    // render
     render() {
         let orders = <Spinner />;
         if (!this.props.isLoading) {
